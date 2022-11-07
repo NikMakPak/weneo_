f42cd8290da70fa6d7c614581058ec6cc7a97ffc
Pr0teL <megakot1337@gmail.com>
2022-11-07 19:50:44 +0300

    Добавил Scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css';
+import React from 'react'
+import Navbar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Start App</h1>
     </div>
   );
