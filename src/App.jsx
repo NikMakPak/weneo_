135e451a5fb04e7602bd4b9538911796faf2f155
Pr0teL <megakot1337@gmail.com>
2022-11-07 21:34:49 +0300

    Доработка header

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+      <img className='logo' width={404} height={464} src="img/logo.svg" alt="" />
         <div className="header">
         <Navbar />
         <div className="wrap">
-          <img className='logo' width={404} height={464} src="img/logo.svg" alt="" />
           <div className="infoBlock">
             <h1>Инновационная веб студия для вашего вашего бизнеса</h1>
             <p>Cоздание нового или редизайне существующего проекта с помощью искусственного интеллекта и нашей современной платформы.</p>
