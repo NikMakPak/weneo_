b0cc3a54b4d608b21343a0e16d05ef89d74c7e50
buldig <mr.karandashin@mail.ru>
2022-11-08 20:53:19 +0300

    Начал разработку блока, строительной единицы сайта

---
+++ src/components/Block/Block.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Block = () => {
+  class Block {
+
+    constructor() {
+      this.width = "100%"
+      this.height = "250px"
+    }
+
+
+  }
+
+  class Button extends Block {
+    constructor(props) {
+      super(props);
+
+    }
+
+  }
+
+  const block = new Block();
+  return (
+    <div style={{
+      background: '#9c9c9c',
+      width: block.width,
+      height: block.height,
+      display: 'flex',
+      justifyContent: 'space-evenly',
+      alignItems: 'center'
+    }}>
+      <div style={{background: '#494949', width: '200px', height: '200px'}}>button</div>
+      <div style={{background: '#494949', width: '200px', height: '200px'}}>text</div>
+      <div style={{background: '#494949', width: '200px', height: '200px'}}>input</div>
+    </div>
+  );
+};
+
+export default Block;
