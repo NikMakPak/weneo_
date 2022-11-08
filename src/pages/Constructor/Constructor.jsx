467272fc74ff39eaef88393ecbe5a58e2c2962a3
Pr0teL <megakot1337@gmail.com>
2022-11-08 20:41:58 +0300

    Добавление сайдбара с селектором

--- src/pages/Constructor/Constructor.jsx
+++ src/pages/Constructor/Constructor.jsx
@@ -2,6 +2,13 @@ import React from "react"
 import styles from './Constructor.module.scss';
 import CapabilityCard from "../../components/CapabilityCard/CapabilityCard";
 export default function Constructor() {
+    const [selectBar, setSelectBar] = React.useState('');
+    const arOptions = ['Шаблоноблоки', 'Нейроноблоки', 'Позициоблоки'];
+    const options = arOptions.map((text, index) => {
+        return <option key={index} value={index}>{text}</option>;
+     });
+  
+ 
     return (<>
         <div className={styles.headerBar}>
             <div className={styles.headLBar}>
@@ -9,37 +16,43 @@ export default function Constructor() {
                 <h3>Название нового сайта</h3>
             </div>
             <div className={styles.headRBar}>
-               <img src="./img/undo.svg" alt="" />
-               <img style={{transform: "rotateY(180deg)"}} src="./img/undo.svg" alt="" />
-               <img src="./img/preview.svg" alt="" />
-               <img src="./img/person_min.svg" alt="" />
-               <img src="./img/settings.svg" alt="" />
-               <div className={styles.barSelector}>
-                 <ul>
-                    <li>Контент</li>
-                    <li>Страницы</li>
-                 </ul>
-               </div>
+                <img src="./img/undo.svg" alt="" />
+                <img style={{ transform: "rotateY(180deg)" }} src="./img/undo.svg" alt="" />
+                <img src="./img/preview.svg" alt="" />
+                <img src="./img/person_min.svg" alt="" />
+                <img src="./img/settings.svg" alt="" />
+                <div className={styles.barSelector}>
+                    <ul>
+                        <li>Контент</li>
+                        <li>Страницы</li>
+                    </ul>
+                </div>
             </div>
         </div>
         <div className={styles.construct}>
             <div className={styles.constrWindow}>
                 {/* Рабочая зона */}
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
             </div>
             <div className={styles.sideBar}>
-123
+            <img width={24} height={24} src="./img/widgets.svg" alt="" />
+            <select value={selectBar} onChange={(event) => setSelectBar(event.target.value)}>
+         {options}
+      </select>
+                <p>
+                    Выбрана опция: {selectBar}
+                </p>
             </div>
 
         </div>
-        </>
+    </>
     )
 }
