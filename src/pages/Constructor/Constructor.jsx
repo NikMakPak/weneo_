0ee6d23c34c4ce589a0d4fcbb140c08bb4721414
Pr0teL <megakot1337@gmail.com>
2022-11-08 19:50:40 +0300

    Добавление рабочей зоны конструктора

--- src/pages/Constructor/Constructor.jsx
+++ src/pages/Constructor/Constructor.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import styles from './Constructor.module.scss';
+import CapabilityCard from "../../components/CapabilityCard/CapabilityCard";
 export default function Constructor() {
-    return (
-        <div className={styles.header_bar}>
-            <div className={styles.lBar}>
+    return (<>
+        <div className={styles.headerBar}>
+            <div className={styles.headLBar}>
                 <img width={36} height={36} src="./img/construct.svg" alt="" />
                 <h3>Название нового сайта</h3>
             </div>
-            <div className={styles.rBar}>
+            <div className={styles.headRBar}>
                <img src="./img/undo.svg" alt="" />
                <img style={{transform: "rotateY(180deg)"}} src="./img/undo.svg" alt="" />
                <img src="./img/preview.svg" alt="" />
@@ -21,5 +22,24 @@ export default function Constructor() {
                </div>
             </div>
         </div>
+        <div className={styles.construct}>
+            <div className={styles.constrWindow}>
+                {/* Рабочая зона */}
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+                <h1 style={{'textAlign': "center", "marginTop": 100}}>Добро пожаловать в инновационный веб конструктор!</h1>
+            </div>
+            <div className={styles.sideBar}>
+123
+            </div>
+
+        </div>
+        </>
     )
 }
