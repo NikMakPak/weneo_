66e19c29eeb6f210fd72adb554c88ef5deae840f
Pr0teL <megakot1337@gmail.com>
2022-11-10 20:15:12 +0300

    Add React DragAndDrop

--- src/components/SidebarBlock/SidebarBlock.jsx
+++ src/components/SidebarBlock/SidebarBlock.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import styles from './SidebarBlock.module.scss'
-
-const SidebarBlock = ({ title, descr }) => {
+import { useDrag } from 'react-dnd'
+import { ItemTypes } from './ItemTypes.js'
+// Поместить объект стилей блока сайдбара в отдельный файл
+const style = {
+    minHeight: "79px",
+    boxShadow: "0px 4px 10px #C2D6DA",
+    padding: "10px 5px 10px 0",
+    boxSizing: "border-box",
+    marginBottom: "10px",
+    transition: "$T_TIME",
+  }
+export const SidebarBlock = function Box({title,descr, setContainerItems}) {
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: ItemTypes.BOX, //Нужно передавать тип блока, с помощью проверок в ConstructWindow Будет генериться нужный блок
+        item: { title, descr },
+        end: (item, monitor) => {
+            const dropResult = monitor.getDropResult()
+            if (item && dropResult) {
+                setContainerItems(perv => [...perv, item])
+              console.log(item);
+            }
+        },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+            handlerId: monitor.getHandlerId(),
+        }),
+    }))
+    const opacity = isDragging ? 0.4 : 1
     return (
-        <div className={styles.block}>
+        <div ref={drag} style={{ ...style, opacity }} data-testid={`box`}>
             <img src='./img/grid_view.svg' alt="icon" />
             <div className={styles.content}>
                 <h4>{title}</h4>
                 <p>{descr}</p>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default SidebarBlock;
--- src/pages/Constructor/Constructor.jsx
+++ src/pages/Constructor/Constructor.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import styles from './Constructor.module.scss';
 import SidebarBlock from "../../components/SidebarBlock/SidebarBlock";
 import Block from "../../components/Blocks/Block/Block";
+import { useDrop, DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { ItemTypes } from "../../components/SidebarBlock/ItemTypes";
 
 export default function Constructor() {
     const arOptions = ['Шаблоноблоки', 'Нейроноблоки', 'Позициоблоки'];
@@ -11,6 +14,49 @@ export default function Constructor() {
     const options = arOptions.map((text, index) => {
         return <option key={index} value={index}>{text}</option>;
     });
+
+    const [containerItems, setContainerItems] = React.useState([])
+
+    // Поместить объект стилей в отдельный файл
+    const style = {
+        overflow: "auto",
+        marginTop: "20px",
+        marginLeft: "5%",
+        width: "75%",
+        height: "90vh",
+        background: "#FFF",
+        boxShadow: "4px 4px 40px rgba(0, 0, 0, 0.25)",
+        transition: "0.3s"
+    }
+//Мб вынести в отдельный модуль
+    const ConstuctWindow = () => {
+        const [{ canDrop, isOver }, drop] = useDrop(() => ({
+            accept: ItemTypes.BOX,
+            drop: () => ({ name: 'Dustbin' }),
+            collect: (monitor) => ({
+                isOver: monitor.isOver(),
+                canDrop: monitor.canDrop(),
+            }),
+        }))
+        const isActive = canDrop && isOver
+        let backgroundColor = '#FFF'
+        if (isActive) {
+            // backgroundColor = 'gray'
+        } else if (canDrop) {
+            backgroundColor = 'rgba(200,200,200, 0.1)'
+        }
+        return (
+            <div ref={drop} style={{ ...style, backgroundColor }} data-testid="dustbin">
+                {/* {isActive ? 'Release to drop' : 'Drag a box here'} */}
+                {
+                    containerItems.length ? containerItems.map((e) => <Block />) :
+                        <h3 style={{ textAlign: "center", marginTop: "20%" }}>Перетащите сюда блок</h3>
+                }
+            </div>
+        )
+    }
+
+
     return (<>
 
         <div className={styles.headerBar}>
@@ -33,41 +79,45 @@ export default function Constructor() {
             </div>
         </div>
         <div className={styles.construct}>
-            <div className={styles.constrWindow}>
-                {/* Рабочая зона */}
-                <h1 style={{ 'textAlign': "center", "marginTop": 100 }}>Добро пожаловать в инновационный веб конструктор!</h1>
-                <Block />
-            </div>
-            <div className={styles.sideBar}>
-                <img style={{ transform: `rotate(${rotateWidg}deg)` }} src="./img/widgets.svg" alt="" />
-                <select value={value} onChange={(event) => { setValue(event.target.value); setRotateWidg(prev => prev + 90) }}>
-                    {options}
-                </select>
-                <div className={styles.sideBarContent}>
-                    {value === "0" ?
-                        <>
-                            <SidebarBlock title={"Шаблоноблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Шаблоноблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Шаблоноблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Шаблоноблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
-                        </>
-                        : value === "1" ?
-                            <>
-                                <SidebarBlock title={"Нейроноблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
-                                <SidebarBlock title={"Нейроноблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
-                                <SidebarBlock title={"Нейроноблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
-                                <SidebarBlock title={"Нейроноблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
-                            </>
-                            : <>
-                            <SidebarBlock title={"Позициоблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Позициоблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Позициоблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
-                            <SidebarBlock title={"Позициоблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
-                        </>
-                        }
-                </div>
-            </div>
+            <DndProvider backend={HTML5Backend}>
 
+
+                <ConstuctWindow />
+
+
+                <div className={styles.sideBar}>
+                    <img style={{ transform: `rotate(${rotateWidg}deg)` }} src="./img/widgets.svg" alt="" />
+                    <select value={value} onChange={(event) => { setValue(event.target.value); setRotateWidg(prev => prev + 90) }}>
+                        {options}
+                    </select>
+                    <div className={styles.sideBarContent}>
+                    {/* Сделать объекты для генерации блоков селектора*/}
+                        {value === "2" ?
+                            <>
+                                <SidebarBlock setContainerItems={setContainerItems} title={"Позициоблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
+                                <SidebarBlock setContainerItems={setContainerItems} title={"Позициоблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
+                                <SidebarBlock setContainerItems={setContainerItems} title={"Позициоблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
+                                <SidebarBlock setContainerItems={setContainerItems} title={"Позициоблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
+                            </>
+                            : value === "1" ?
+                                <>
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Нейроноблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Нейроноблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Нейроноблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Нейроноблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
+                                </>
+                                :
+                                <>
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Шаблоноблок 1"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Шаблоноблок 2"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Шаблоноблок 3"} descr={"описание что делает блок, с текстом и описанием"} />
+                                    <SidebarBlock setContainerItems={setContainerItems} title={"Шаблоноблок 4"} descr={"описание что делает блок, с текстом и описанием"} />
+                                </>
+
+                        }
+                    </div>
+                </div>
+            </DndProvider>
         </div>
     </>
     )
