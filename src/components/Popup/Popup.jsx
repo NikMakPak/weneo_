7f5ffc7680e25bd60eb24b5021292e2737a86f2d
Pr0teL <megakot1337@gmail.com>
2022-11-11 21:09:29 +0300

    add styled popup

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Popup.module.scss'
 
-const Popup = ({active, setActive, kind: kindMiniBlock, changeKind}) => {
+const Popup = ({ active, setActive, kind: kindMiniBlock, changeKind }) => {
   return (
     <div onClick={(e) => {
       e.stopPropagation()
@@ -17,9 +17,19 @@ const Popup = ({active, setActive, kind: kindMiniBlock, changeKind}) => {
           e.stopPropagation()
         }
         }>
-          <button onClick={() => changeKind('input')}>Форма</button>
-          <button onClick={() => changeKind('text')}>Блок текста</button>
-          <button onClick={() => changeKind('button')}>Кнопка</button>
+          <div className={styles.modalSwitch}>
+            <div className={styles.Switcher}>
+              <img src="./img/tune.svg" alt="" />
+            </div>
+            <div className={styles.Switcher}>
+              <img src="./img/format_size.svg" alt="" />
+            </div>
+          </div>
+          <div className={styles.sattingsWrap}>
+            <button onClick={() => changeKind('input')}>Форма</button>
+            <button onClick={() => changeKind('text')}>Блок текста</button>
+            <button onClick={() => changeKind('button')}>Кнопка</button>
+          </div>
         </div>
       </div>
     </div>
