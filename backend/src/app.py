96e07daf17b5cb4fb48c93bd79f76d42280a26c2
nikmakpak <nicement28@gmail.com>
2022-11-11 20:56:59 +0300

    feat: add userAdd to db , add api for load Bundle to db, add neural networks txt api

--- backend/src/app.py
+++ backend/src/app.py
@@ -10,9 +10,39 @@ CORS(app)
 
 db=mongo.db.users
 
-@app.route("/")
-def index():
- return '<h1>Hello from server</h1>'
+@app.route("/users",methods=['POST'])
+def createUser():
+    id = db.insert({
+        'name': request.json['name'],
+        'email':request.json['email'],
+        'phone':request.json['phone'],
+        'project-bundle':''
+    })
+    return jsonify({'id':str(ObjectId(id)), 'msg'= "User added"})
+
+
+@app.route('/upload-user-bundle/<id>', methods=['POST'])
+def uploadBundle(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    user['project-bundle'] = request.json['project-bundle']
+    return jsonify({'msg' = "Bundle uploaded"})
+
+@app.route('/download-user-bundle/<id>', methods=['GET'])
+def downloadBundle(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    return jsonify({'project-bundle':user['project-bundle']})
+
+@app.route("/nlp")
+def text_nlp():
+    text = request.json['text_analize']
+    if(text!=''):
+        from rutermextract import TermExtractor
+        term_extractor = TermExtractor()
+        dict = {}
+        for term in term_extractor(text):
+           dict.update({term.normalized:term.count})
+        return jsonify({"answer": dict})
+    return jsonify({"answer": False})
 
 if __name__ == '__main__':
     app.run(debug=True)
