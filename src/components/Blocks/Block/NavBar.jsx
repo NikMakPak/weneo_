edb0055b2b48e2ea89178358f39ab484a32f6a56
Pr0teL <megakot1337@gmail.com>
2022-11-18 19:39:37 +0300

    Аdd preview mode, add functional in navbar block

--- src/components/Blocks/Block/NavBar.jsx
+++ src/components/Blocks/Block/NavBar.jsx
@@ -1,8 +1,25 @@
 import React from "react"
+import Popup from "../../Popup/Popup";
 
-export default function NavBar({}) {
-    return(
-        <div style={{
+export default function NavBar({ Items, setContainerItems, containerItems }) {
+    console.log(Items);
+    const [modalActive, setModalActive] = React.useState(false)
+    const [modalPos, setModalPos] = React.useState({})
+    const [blockStyles, setBlockStyles] = React.useState(Items.elements)
+    const onClickEdit = (event) => {
+        console.log(event);
+        setModalPos({ posX: event.pageX, posY: event.pageY })
+        setModalActive(true)
+
+    }
+
+
+
+    return (
+        <div onClick={onClickEdit} style={{
+           
+        }}>
+            <div style={{
             padding: "20px",
             background: '#FFF',
             width: "100%",
@@ -10,24 +27,28 @@ export default function NavBar({}) {
             display: 'flex',
             justifyContent: "space-between",
             alignItems: "center"
-          }}>
-            <img width={30} height={30} src="./img/construct.svg" alt="" />
-            <ul style={{
-                display: "flex",
-                listStyle: "none",
-                width: "300px",
-                justifyContent: "space-around"
-            }}>
-                <li style={{
-                    cursor: "pointer",
-                }}>О нас</li>
-                <li style={{
-                    cursor: "pointer",
-                }}>Технологии</li>
-                <li style={{
-                    cursor: "pointer",
-                }}>Заказать</li>
-            </ul>
+        }}>
+                <img width={30} height={30} src="./img/construct.svg" alt="" />
+                <ul style={{
+                    display: "flex",
+                    listStyle: "none",
+                 
+                }}>
+                    <li style={{
+                        cursor: "pointer",
+                        marginRight: "20px"
+                    }}>{blockStyles.li1.val}</li>
+                    <li style={{
+                        cursor: "pointer",
+                        marginRight: "20px"
+                    }}>{blockStyles.li2.val}</li>
+                    <li style={{
+                        cursor: "pointer",
+                    }}>{blockStyles.li3.val}</li>
+                </ul>
+            </div>
+
+            <Popup Items={Items} containerItems={containerItems} setContainerItems={setContainerItems} active={modalActive} setActive={() => setModalActive()} pos={modalPos} blockStyles={blockStyles} setBlockStyles={(e) => setBlockStyles(e)} />
         </div>
     )
 }
--- src/pages/Constructor/Constructor.jsx
+++ src/pages/Constructor/Constructor.jsx
@@ -14,16 +14,16 @@ import TitleBlock from "../../components/Blocks/TitleBlock";
 
 export default function Constructor() {
     const [value, setValue] = React.useState([]);
-
+    const [preview, setPreview] = React.useState(false);
     const [containerItems, setContainerItems] = React.useState([]) // Глобавльный стейт элементов
 
     // Поместить объект стилей в отдельный файл
     const style = {
-        overflow: "auto",
-        marginTop: "20px",
-        marginLeft: "5%",
-        width: "75%",
-        height: "90vh",
+        overflow: preview ? "" : "auto",
+        marginTop: preview ? "" :"20px",
+        marginLeft: preview ? "" : "5%",
+        width: preview ? "100%" :"75%",
+        height: preview ? "" :"90vh",
         background: "#FFF",
         boxShadow: "4px 4px 40px rgba(0, 0, 0, 0.25)",
         transition: "0.3s"
@@ -52,10 +52,10 @@ export default function Constructor() {
                 {
                     containerItems.length ? containerItems.map((e) =>
                         e.kind === "form" ? <FormBlock /> :
-                            e.kind === "nav" ? <NavBar /> :
-                                e.kind === "header" ? <Header containerItems={containerItems} setContainerItems={(obj)=> setContainerItems(obj)} Items={e}/> :
-                                    e.kind === "title" ? <TitleBlock containerItems={containerItems} setContainerItems={(obj)=> setContainerItems(obj)} Items={e}/> :
-                                        e.kind === "text" ? <TextBlock containerItems={containerItems} setContainerItems={(obj)=> setContainerItems(obj)} Items={e} /> :
+                            e.kind === "nav" ? <NavBar containerItems={containerItems} setContainerItems={(obj) => setContainerItems(obj)} Items={e} /> :
+                                e.kind === "header" ? <Header containerItems={containerItems} setContainerItems={(obj) => setContainerItems(obj)} Items={e} /> :
+                                    e.kind === "title" ? <TitleBlock containerItems={containerItems} setContainerItems={(obj) => setContainerItems(obj)} Items={e} /> :
+                                        e.kind === "text" ? <TextBlock containerItems={containerItems} setContainerItems={(obj) => setContainerItems(obj)} Items={e} /> :
                                             <></>) :
                         <h3 style={{ textAlign: "center", marginTop: "20%" }}>Перетащите сюда блок</h3>
                 }
@@ -64,128 +64,144 @@ export default function Constructor() {
     }
 
 
-    return (<>
-
-        <div className={styles.headerBar}>
-            <div className={styles.headLBar}>
-                <img width={36} height={36} src="./img/construct.svg" alt="" />
-                <h4 style={{fontSize: "18px"}}>Название нового сайта</h4>
-            </div>
-            <div className={styles.headRBar}>
-                <img src="./img/undo.svg" alt="" />
-                <img style={{ transform: "rotateY(180deg)" }} src="./img/undo.svg" alt="" />
-                <img src="./img/preview.svg" alt="" />
-                <img src="./img/person_min.svg" alt="" />
-                <img src="./img/settings.svg" alt="" />
-                <div className={styles.barSelector}>
-                    <ul>
-                        <li>Контент</li>
-                        <li>Страницы</li>
-                    </ul>
+    return (
+        preview === false ?
+            <>
+                <div className={styles.headerBar}>
+                    <div className={styles.headLBar}>
+                        <img width={36} height={36} src="./img/construct.svg" alt="" />
+                        <h4 style={{ fontSize: "18px" }}>Название нового сайта</h4>
+                    </div>
+                    <div className={styles.headRBar}>
+                        <img src="./img/undo.svg" alt="" />
+                        <img style={{ transform: "rotateY(180deg)" }} src="./img/undo.svg" alt="" />
+                        <img onClick={() => setPreview(true)} src="./img/preview.svg" alt="" />
+                        <img src="./img/person_min.svg" alt="" />
+                        <img src="./img/settings.svg" alt="" />
+                        <div className={styles.barSelector}>
+                            <ul>
+                                <li>Контент</li>
+                                <li>Страницы</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div className={styles.construct}>
-            <DndProvider backend={HTML5Backend}>
+                <div className={styles.construct}>
+                    <DndProvider backend={HTML5Backend}>
 
 
-                <ConstuctWindow />
+                        <ConstuctWindow />
 
 
-                <div className={styles.sideBar}>
-                    {/* <img style={{ transform: `rotate(${rotateWidg}deg)` }} src="./img/widgets.svg" alt="" />
+                        <div className={styles.sideBar}>
+                            {/* <img style={{ transform: `rotate(${rotateWidg}deg)` }} src="./img/widgets.svg" alt="" />
                     <select value={value} onChange={(event) => { setValue(event.target.value); setRotateWidg(prev => prev + 90) }}>
                         {options}
                     </select> */}
-                    <div onClick={() => setValue((prev) => !prev.includes("0") ? [...prev, "0"] : prev.filter((e) => e !== "0"))} className={styles.select}>
-                        <img src="./img/widgets.svg" alt="" />
-                        <h4>Формы</h4>
-                        <img width={14} height={11} src="./img/select-arr.svg" alt="" />
-                    </div>
-                    <div className={styles.sideBarContent}>
-                        {
-                            value.includes("0") &&
-                            <>
+                            <div onClick={() => setValue((prev) => !prev.includes("0") ? [...prev, "0"] : prev.filter((e) => e !== "0"))} className={styles.select}>
+                                <img src="./img/widgets.svg" alt="" />
+                                <h4>Формы</h4>
+                                <img width={14} height={11} src="./img/select-arr.svg" alt="" />
+                            </div>
+                            <div className={styles.sideBarContent}>
+                                {
+                                    value.includes("0") &&
+                                    <>
 
-                                <SidebarBlock kind={"form"} setContainerItems={setContainerItems} title={"Стандартная Форма"} descr={"Основной блок с формой"} />
-                            </>
-                        }
-                    </div>
-                    <div onClick={() => setValue((prev) => !prev.includes("1") ? [...prev, "1"] : prev.filter((e) => e !== "1"))} className={styles.select}>
-                        <img src="./img/widgets.svg" alt="" />
-                        <h4>Навигация</h4>
-                        <img width={14} height={11} src="./img/select-arr.svg" alt="" />
-                    </div>
-                    <div className={styles.sideBarContent}>
-                        {
-                            value.includes("1") &&
-                            <>
-                                <SidebarBlock setContainerItems={setContainerItems} kind={"nav"} title={"Стандартная навигация"} descr={"Блок с навигацией"} />
-                            </>
-                        }
-                    </div>
-                    <div onClick={() => setValue((prev) => !prev.includes("2") ? [...prev, "2"] : prev.filter((e) => e !== "2"))} className={styles.select}>
-                        <img src="./img/widgets.svg" alt="" />
-                        <h4>Шапки</h4>
-                        <img width={14} height={11} src="./img/select-arr.svg" alt="" />
-                    </div>
-                    <div className={styles.sideBarContent}>
-                        {
-                            value.includes("2") &&
+                                        <SidebarBlock kind={"form"} setContainerItems={setContainerItems} title={"Стандартная Форма"} descr={"Основной блок с формой"} />
+                                    </>
+                                }
+                            </div>
+                            <div onClick={() => setValue((prev) => !prev.includes("1") ? [...prev, "1"] : prev.filter((e) => e !== "1"))} className={styles.select}>
+                                <img src="./img/widgets.svg" alt="" />
+                                <h4>Навигация</h4>
+                                <img width={14} height={11} src="./img/select-arr.svg" alt="" />
+                            </div>
+                            <div className={styles.sideBarContent}>
+                                {
+                                    value.includes("1") &&
+                                    <>
+                                        <SidebarBlock
+                                            setContainerItems={setContainerItems}
+                                            kind={"nav"}
+                                            title={"Стандартная навигация"}
+                                            descr={"Блок с навигацией"}
+                                            elements={{
+                                                li1: { val: "О нас" }, li2: { val: "Технологии" }, li3: { val: "Заказать" },
+                                            }} />
+                                    </>
+                                }
+                            </div>
+                            <div onClick={() => setValue((prev) => !prev.includes("2") ? [...prev, "2"] : prev.filter((e) => e !== "2"))} className={styles.select}>
+                                <img src="./img/widgets.svg" alt="" />
+                                <h4>Шапки</h4>
+                                <img width={14} height={11} src="./img/select-arr.svg" alt="" />
+                            </div>
+                            <div className={styles.sideBarContent}>
+                                {
+                                    value.includes("2") &&
 
-                            <>
-                                <SidebarBlock
-                                    kind={"header"}
-                                    containerItems={containerItems}
-                                    setContainerItems={setContainerItems}
-                                    title={"Стандартная шапка"}
-                                    descr={"Блок с стандартной шапкой"}
-                                    elements={{
-                                         h1: {val: "Сайт для вашего бизнеса", fontSize: "45px", color: "#FFF",},
-                                          h4: {val: "Добавьте интересные подробности о вашей компании. Кликом на блок можно изменить его наполнение или настроить стили.", fontSize: "20px", color: "#FFF"},
-                                           btn: {val: "Создать",fontSize: "15px", bg: "#2971F5", color: "#FFF"} 
-                                           }} />
-                            </>
-                        }
-                    </div>
-                    <div onClick={() => setValue((prev) => !prev.includes("3") ? [...prev, "3"] : prev.filter((e) => e !== "3"))} className={styles.select}>
-                        <img src="./img/widgets.svg" alt="" />
-                        <h4>Текст</h4>
-                        <img width={14} height={11} src="./img/select-arr.svg" alt="" />
-                    </div>
-                    <div className={styles.sideBarContent}>
-                        {
-                            value.includes("3") &&
+                                    <>
+                                        <SidebarBlock
+                                            kind={"header"}
+                                            containerItems={containerItems}
+                                            setContainerItems={setContainerItems}
+                                            title={"Стандартная шапка"}
+                                            descr={"Блок с стандартной шапкой"}
+                                            elements={{
+                                                h1: { val: "Сайт для вашего бизнеса", fontSize: "45px", color: "#FFF", },
+                                                h4: { val: "Добавьте интересные подробности о вашей компании. Кликом на блок можно изменить его наполнение или настроить стили.", fontSize: "20px", color: "#FFF" },
+                                                btn: { val: "Создать", fontSize: "15px", bg: "#2971F5", color: "#FFF" }
+                                            }} />
+                                    </>
+                                }
+                            </div>
+                            <div onClick={() => setValue((prev) => !prev.includes("3") ? [...prev, "3"] : prev.filter((e) => e !== "3"))} className={styles.select}>
+                                <img src="./img/widgets.svg" alt="" />
+                                <h4>Текст</h4>
+                                <img width={14} height={11} src="./img/select-arr.svg" alt="" />
+                            </div>
+                            <div className={styles.sideBarContent}>
+                                {
+                                    value.includes("3") &&
 
-                            <>
-                                <SidebarBlock 
-                                kind={"title"} 
-                                setContainerItems={setContainerItems} 
-                                title={"Стандартная заголовок"} 
-                                descr={"Блок с стандартным заголовком"} 
-                                elements={{
-                                    h1: {val: "Заголовок", fontSize: "35px", color: "#000",},
-                                      }}/>
+                                    <>
+                                        <SidebarBlock
+                                            kind={"title"}
+                                            setContainerItems={setContainerItems}
+                                            title={"Стандартная заголовок"}
+                                            descr={"Блок с стандартным заголовком"}
+                                            elements={{
+                                                h1: { val: "Заголовок", fontSize: "35px", color: "#000", },
+                                            }} />
 
-                                <SidebarBlock 
-                                kind={"text"} 
-                                setContainerItems={setContainerItems} 
-                                title={"Стандартный текст"} 
-                                descr={"Блок с стандартным текстом"} 
-                                elements={{
-                                         p: {val: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur ut ea dignissimos, mollitia a. Eaque a ducimus, tenetur temporibus dolore perspiciatis veritatis exercitationem rem quidem natus et numquam? Quisquam.", fontSize: "25px", color: "#000",},
-                                           }} />
-                                           
-                            </>
-                        }
-                    </div>
+                                        <SidebarBlock
+                                            kind={"text"}
+                                            setContainerItems={setContainerItems}
+                                            title={"Стандартный текст"}
+                                            descr={"Блок с стандартным текстом"}
+                                            elements={{
+                                                p: { val: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo pariatur ut ea dignissimos, mollitia a. Eaque a ducimus, tenetur temporibus dolore perspiciatis veritatis exercitationem rem quidem natus et numquam? Quisquam.", fontSize: "25px", color: "#000", },
+                                            }} />
+
+                                    </>
+                                }
+                            </div>
 
 
-                    {/* Сделать объекты для генерации блоков селектора*/}
+                            {/* Сделать объекты для генерации блоков селектора*/}
+                        </div>
+
+                    </DndProvider>
                 </div>
+            </>
+            : <>
+                <h5 onClick={() => setPreview(false)} style={{textAlign:"center", cursor: "pointer", margin: "5px 0"}}>Завершить просмотр</h5>
+                <DndProvider backend={HTML5Backend}>
+                    <ConstuctWindow/>
+                </DndProvider>
 
-            </DndProvider>
-        </div>
-    </>
+            </>
+
     )
 }
